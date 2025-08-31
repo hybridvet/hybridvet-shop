f694ca617735d9ee64c7a4e00d7db3bbcab9d3cc
hybridvet <ivan.tomic97@gmail.com>
2025-08-31 21:00:59 +0200

    Create LinkInBioShop.jsx

---
+++ components/LinkInBioShop.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function LinkInBioShop() {
+  return (
+    <div style={{minHeight:"100vh",display:"grid",placeItems:"center",fontFamily:"sans-serif"}}>
+      <h1>Radi! /planovi je online ✅</h1>
+    </div>
+  );
+}
